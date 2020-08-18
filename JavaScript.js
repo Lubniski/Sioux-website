42d9fa520d3343ce82407991dfe8d4b6059c7f4c
Lubniski <69198731+Lubniski@users.noreply.github.com>
2020-08-18 09:29:49 -0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,30 @@
+var num=1;
+
+function adelante(){
+	num++;
+	if(num>5){
+		num=1;
+			var Jean=document.getElementById('Jean');
+		Jean.src="Jean"+num+".Jean";
+	<img src="Jean.PNG" alt="Jean" width="100" height="150" id="Jean">
+	<img src="Jean1.PNG" alt="Jean1" width="100" height="150" id="Jean">
+	<img src="Jean2.PNG" alt="Jean2" width="100" height="150" id="Jean">
+	<img src="Jean3.PNG" alt="Jean3" width="100" height="150" id="Jean">
+	<img src="Jean4.PNG" alt="Jean4" width="100" height="150" id="Jean">
+	}
+}
+
+function atrás(){
+	num--;
+	if(num>1){
+		num=5;
+		var Jean=document.getElementById('Jean');
+		Jean.src="Jean"+num+".Jean";
+	<img src="Jean.PNG" alt="Jean" width="100" height="150" id="Jean">
+	<img src="Jean1.PNG" alt="Jean1" width="100" height="150" id="Jean">
+	<img src="Jean2.PNG" alt="Jean2" width="100" height="150" id="Jean">
+	<img src="Jean3.PNG" alt="Jean3" width="100" height="150" id="Jean">
+	<img src="Jean4.PNG" alt="Jean4" width="100" height="150" id="Jean">
+	}
+}
+
