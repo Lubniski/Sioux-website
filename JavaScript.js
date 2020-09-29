f16b64e840a4dd6174c2fada0308bff08022b1fe
Lubniski <69198731+Lubniski@users.noreply.github.com>
2020-09-29 09:28:12 -0300

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -3,28 +3,63 @@ var num=1;
 function adelante(){
 	num++;
 	if(num>5){
-		num=1;
+		num=1;}
+		
 			var Jean=document.getElementById('Jean');
 		Jean.src="Jean"+num+".Jean";
-	<img src="Jean.PNG" alt="Jean" width="100" height="150" id="Jean">
-	<img src="Jean1.PNG" alt="Jean1" width="100" height="150" id="Jean">
-	<img src="Jean2.PNG" alt="Jean2" width="100" height="150" id="Jean">
-	<img src="Jean3.PNG" alt="Jean3" width="100" height="150" id="Jean">
-	<img src="Jean4.PNG" alt="Jean4" width="100" height="150" id="Jean">
-	}
-}
 
 function atrás(){
 	num--;
 	if(num>1){
-		num=5;
+		num=5;}
 		var Jean=document.getElementById('Jean');
 		Jean.src="Jean"+num+".Jean";
-	<img src="Jean.PNG" alt="Jean" width="100" height="150" id="Jean">
-	<img src="Jean1.PNG" alt="Jean1" width="100" height="150" id="Jean">
-	<img src="Jean2.PNG" alt="Jean2" width="100" height="150" id="Jean">
-	<img src="Jean3.PNG" alt="Jean3" width="100" height="150" id="Jean">
-	<img src="Jean4.PNG" alt="Jean4" width="100" height="150" id="Jean">
+
+		var slideIndex = 0;
+showSlides();
+
+
+const searchBtn=document.querySelector(".search-btn");
+const searchOverlay=document.querySelector(".search-overlay");
+const searchPopup=document.querySelector(".seasrch-popup");
+
+searchBtn.addEventListener("click",()=>{
+	searchOverlay.classlist.add("active");
+	searchPopup.classlist.add("active");
+})
+
+searchOverlay.addEventListener("click",(event))=>{
+	if(event.target ==searchOverlay){
+	searchOverlay.classlist.remove("active");
+	searchPopup.classlist.remove("active");
 	}
 }
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
